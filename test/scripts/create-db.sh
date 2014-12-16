a24dded25e3c79ada8d0bce1219321984a75fe61
Shruti Sharma <shrutijalewar@gmail.com>
2014-12-16 13:43:43 -0600

    corrected create-db

--- test/scripts/create-db.sh
+++ test/scripts/create-db.sh
@@ -9,10 +9,10 @@ psql $1 -f ../../db/tables/users.sql
 psql $1 -f ../../db/tables/notes.sql
 psql $1 -f ../../db/tables/tags.sql
 psql $1 -f ../../db/tables/photos.sql
-psql $1 -f ../../db/tables/notes-tags.sql
+psql $1 -f ../../db/tables/notes_tags.sql
 
 
-psql $1 -f ../../db/functions/add-note.sql
-psql $1 -f ../../db/functions/nuke-note.sql
-psql $1 -f ../../db/functions/show-note.sql
-psql $1 -f ../../db/functions/query-notes.sql
+psql $1 -f ../../db/functions/add_note.sql
+psql $1 -f ../../db/functions/nuke_note.sql
+psql $1 -f ../../db/functions/show_note.sql
+psql $1 -f ../../db/functions/query_notes.sql
